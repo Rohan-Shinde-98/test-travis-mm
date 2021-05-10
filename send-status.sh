3230de8b7d29840dd2f03ef5229cee2ced6e9578
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-05-10 15:43:55 +0530

    Update send-status.sh

--- send-status.sh
+++ send-status.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' https://test-mm.cloud.mattermost.com/hooks/hpwmkw869iyb5fa5sk8rbimkny
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' $MM_WEBHOOK_URL
