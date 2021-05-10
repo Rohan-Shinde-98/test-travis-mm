05bcfdf9eb36aadf519cb752044286d43eacd4af
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-05-10 15:46:59 +0530

    Update send-status.sh

--- send-status.sh
+++ send-status.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' $MM_WEBHOOK_URL
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Current Build number : $TRAVIS_JOB_ID. :tada:"}' $MM_WEBHOOK_URL
